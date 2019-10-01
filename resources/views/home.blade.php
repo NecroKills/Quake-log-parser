47539a854d3b44228f272dcc254dc433c8a774bc
maycon da silva moreira <maycondasilvamoreira@hotmail.com>
2019-10-01 17:09:27 -0300

    adicionado um botão submit para subir o arquivo games.log e adcionado a rota ao form

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,27 +1,34 @@
 @extends('layout.site')
 
-@section('titulo', 'Cursos')
+@section('titulo', 'Quake Log Parser')
 
 @section('conteudo')
 
-<div class="container">
+<div class="container ">
   <div class="row">
     <div class="col s6 offset-s3 valign">
-      <div class="card center">
-        <div class="card-center">
+      <div class="card center #f5f5f5 grey lighten-4">
+        <div class="card-center ">
           <h4 class="center">Quake Log Parser</h4>
         </div>
         <div class="card-action">
-          <form action="#">
+          <form class="" action="{{route('log.selecionar')}}" method="POST"
+                enctype="multipart/form-data">
+            {{ csrf_field() }}
             <div class="file-field input-field">
               <div class="btn">
                 <span>Select File</span>
-                <input type="file" multiple>
+                <input type="file" name="file" id="file">
               </div>
               <div class="file-path-wrapper">
-              <input class="file-path validate" type="text" placeholder="Selecione o arquivo de log do game">
+                <input class="file-path validate" type="text"
+                  placeholder="Selecione o arquivo de log do game">
               </div>
             </div>
+            <button class="btn waves-effect waves-light" type="submit"
+                    name="action">OK
+              <i class="material-icons right">send</i>
+            </button>
           </form>
         </div>
       </div>
