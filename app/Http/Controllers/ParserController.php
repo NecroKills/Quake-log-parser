8ab852d992f431986988c0ef93262568733bf48f
maycon da silva moreira <maycondasilvamoreira@hotmail.com>
2019-10-01 17:00:23 -0300

    Criado o metodo selecionar, que recebe a request e chama o metodo analisador

--- app/Http/Controllers/ParserController.php
+++ app/Http/Controllers/ParserController.php
@@ -2,9 +2,34 @@
 
 namespace App\Http\Controllers;
 
+use App\Business\ParserLogBusiness;
 use Illuminate\Http\Request;
+use Exception;
 
 class ParserController extends Controller
 {
-    z
+  /**
+   * @var ParserLogBusiness
+   */
+  private $parserLogBusiness;
+
+  /**
+   * ParserController constructor.
+   *
+   * @param ParserLogBusiness $parserLogBusiness
+   */
+  public function __construct(ParserLogBusiness $parserLogBusiness)
+  {
+      $this->parserLogBusiness = $parserLogBusiness;
+  }
+
+  /**
+   * [selecionar description]
+   * @param  Request $req [description]
+   * @return [type]       [description]
+   */
+    public function selecionar(Request $req){
+      $req->all();
+      $registro = $this->parserLogBusiness->analisador($req);
+    }
 }
