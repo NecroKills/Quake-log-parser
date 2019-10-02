5ef787d7489cad73f7e3a5ce925b6ebb0e4f4e21
maycon da silva moreira <maycondasilvamoreira@hotmail.com>
2019-10-02 17:17:15 -0300

    Adicionado o metodo construir json

--- app/Model/Game.php
+++ app/Model/Game.php
@@ -6,46 +6,58 @@ namespace App\Model;
  * [Game description]
  * Sera armazenado as informações que forem encontradas durante a leitura do log
  */
-class Game {
-  private $id;
-  private $total_kills;
-  private $players = array();
-  private $kills = array();
 
-  public function getId() {
-      return $this->id;
-  }
+ class Game {
 
-  public function setId($id) {
-      $this->id = $id;
-  }
+     private $id;
+     private $total_kills;
+     private $players = array();
+     private $kills = array();
 
-  public function getTotal_kills() {
-      return is_null($this->total_kills) ? 0 : $this->total_kills;
-  }
+     public function getId() {
+         return $this->id;
+     }
 
-  public function setTotal_kills($total_kills) {
-      $this->total_kills = $total_kills;
-  }
+     public function getTotal_kills() {
+         return is_null($this->total_kills) ? 0 : $this->total_kills;
+     }
 
-  public function getPlayers() {
-      return $this->players;
-  }
+     public function getPlayers() {
+         return $this->players;
+     }
 
-  public function setPlayers($players) {
-      $this->players[] = $players;
-  }
+     public function getKills() {
+         return $this->kills;
+     }
 
-  public function getKills() {
-      return $this->kills;
-  }
+     public function setId($id) {
+         $this->id = $id;
+     }
 
-  public function setKills($kills) {
-      $this->kills = $kills;
-  }
+     public function setTotal_kills($total_kills) {
+         $this->total_kills = $total_kills;
+     }
 
-  public function addKill() {
-    $this->setTotal_kills($this->getTotal_kills() + 1);
-  }
+     public function setPlayers($players) {
+         $this->players[] = $players;
+     }
 
-}
+     public function setKills($kills) {
+         $this->kills = $kills;
+     }
+
+     public function addKill() {
+         $this->setTotal_kills($this->getTotal_kills() + 1);
+     }
+
+     public function JsonConstruct() {
+         $data = array(
+             'total_kills' => $this->getTotal_kills(),
+             'players' => $this->getPlayers(),
+             'kills' => $this->getKills()
+         );
+         $game = array('game_' . $this->getId() => $data);
+         return json_encode($game, JSON_PRETTY_PRINT);
+     }
+
+ }
